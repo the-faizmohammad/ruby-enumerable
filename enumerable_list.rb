89c2b7fe813053033cc392c2bd2bbb60457bc7c6
Brighton <mtandibrighton0102@gmail.com>
2023-12-01 20:30:05 +0200

    Update enumerable_list.rb

--- enumerable_list.rb
+++ enumerable_list.rb
@@ -1,1 +1,12 @@
+require_relative 'enumerable_module'
+class MyList
+  include MyEnumerable
 
+  def initialize(*enummembers)
+    @list = enummembers
+  end
+
+  def each(&enum_methods)
+    @list.each(&enum_methods)
+  end
+end
